6fb40479b43d513937f2adaf5a12ae1a5092e9a6
Samuel Ampofo <samuel.ampofo@neosinnovation.com>
2018-09-26 10:29:27 +0200

    new amzing stuff

---
+++ myfittnesspal.py
@@ -0,0 +1,36 @@
+
+#imprt jason here 
+import json 
+num_cookies = input("Hey fitness  pal how many chocolate Cokies did you had today ")
+
+#calories per cooky
+calorie = 75
+fat = 3 
+cholesterol = 0 
+carbohydrates =12 
+fiber = 1.5 
+protein = 1.5 
+
+# calories calculation
+amount_cookies = str(num_cookies)
+new_calorie = 75 * num_cookies
+new_fat = 3 * num_cookies
+new_cholesterol = 0 * num_cookies
+new_carbohydrates =12 * num_cookies
+new_fiber = 1.5 * num_cookies
+new_protein = 1.5 * num_cookies
+
+
+#cokkie dictionary
+cookie_calories = {
+  "Calories": new_calorie,
+  "Fat": new_fat,
+  "Cholesterol": new_cholesterol,
+  "Carbohydrates":new_carbohydrates,
+  "Fiber": new_fiber,
+  "Protein": new_protein
+}
+
+#print commands
+print("Hey fittnes pal today you had " + " " + amount_cookies + " Chocolate cookies this is equivalenet to " )
+print(json.dumps(cookie_calories,indent =4))
---
+++ tests/name.py
@@ -0,0 +1,2 @@
+name_user = input("what your name")
+print(name_user)
