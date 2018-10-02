bf8b72c6f1e8dd8fa6ca68efccecf89a84185761
Samuel Ampofo <samuel.ampofo@neosinnovation.com>
2018-10-02 09:53:50 +0200

    assignments great here

---
+++ friendscookies.py
@@ -0,0 +1,17 @@
+#friends list and cookies
+# frinds list exmaple
+myfriend = ["John","Emmanuel","Jame"]
+frinds_snack = []
+
+for name in myfriend:
+    print(name)
+    # print(myfriend.index(name))
+    namesnack = input("what is the favorite snack of" +" "+ name)
+    frinds_snack.append(namesnack)
+    print(name + " likes " + namesnack) 
+    
+for name in myfriend:
+    index = 0
+    index = index + 1
+    snack = frinds_snack[index]
+    print(name + " likes " + snack)
