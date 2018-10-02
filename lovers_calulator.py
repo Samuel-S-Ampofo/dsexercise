7165c4338950acbf8041ee6a0a4d4ded90b05389
Samuel Ampofo <samuel.ampofo@neosinnovation.com>
2018-10-02 09:27:12 +0200

    doing assignmenet here

---
+++ lovers_calulator.py
@@ -0,0 +1,15 @@
+your_name = input("eneter name of person one")
+partner_name = input("eneter name of person two")
+
+#convert to lowercase
+
+#  equal
+
+# greater
+
+#lesser
+
+print(your_name)
+print(partner_name)
+
+print("have a happy union")
